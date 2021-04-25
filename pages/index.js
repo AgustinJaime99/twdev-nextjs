900a0f32550e7a3f2df64a18ecc10da6592b575a
AgustinJaime99 <71357291+AgustinJaime99@users.noreply.github.com>
2021-04-24 21:41:05 -0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,53 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import AppLayout from '../components/AppLayout'
+// devit
 
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Twdev</title>
+        <title>Twe-dev 🐦</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+
+      <AppLayout>
+        <h1>
+          <a href="https://nextjs.org">devter</a>
         </h1>
-    </div>
+        <nav>
+          <Link href='/timeline'>
+            <a>
+              timeline
+            </a>
+          </Link>
+        </nav>
+      </AppLayout>
+
+      <style jsx>{`
+        h1 {
+          text-align: center;
+          font-size: 48px;
+        }
+
+        nav {
+          font-size: 24px;
+          text-align: center;
+        }
+
+        .another-title {
+          color: #333;
+          font-size: 24px;
+        }
+
+        a {
+          color: orange;
+          text-decoration: none;
+        }
+      `}</style>
+    </>
   )
 }
---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+export const breakpoints = {
+    mobile: '520px'
+  }
+  
+  export const fonts = {
+    base: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif'
+  }
+  
+  export const colors = {
+    grey: '#2e2e2e',
+    white: '#ffffff',
+    primary: '#000000',
+    secondary: '#00ffd5'
+  }
